da971395a4296245e3e3b97872e2cadde665f8ce
mrrasdev <las35218@gmail.com>
2024-02-06 12:37:01 +0300

    Create project
    create, setting conn/auth db

---
+++ server/index.js
@@ -0,0 +1,16 @@
+require(`dotenv`).config()
+const express = require("express")
+
+const PORT = process.env.PORT || 5008
+const sequelize = require('./db')
+const app = express()
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+start()
