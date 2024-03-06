e06dcc22f6059b3758b69ce70b967aecff05a126
ras <las35218@gmail.com>
2024-03-06 15:56:16 +0300

    test config server res

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 require(`dotenv`).config()
 const express = require("express")
-
-const PORT = process.env.PORT || 5008
 const sequelize = require('./db')
+const monule = require('./models/models')
+const PORT = process.env.PORT || 5008
+const cors = require('cors')
 const app = express()
+app.use(cors())
+app.use(express.json)
+app.get('/',(req, res)=>{
+    //res.status(200).json({message: 'messagge'})
+    res.end( 'messagge')
+})
 const start = async () => {
     try {
         await sequelize.authenticate()
--- server/models/models.js
+++ server/models/models.js
@@ -1,16 +1,12 @@
 const { DataTypes } = require(`sequelize`)
-const sequelize = require('./db')
-
-// const User = sequelize.define(modelName: 'user', attributes:{
-//     id: {type:DataTypes.INTEGER, primaryKey: true, autoIncrement:true}
-// })
+const sequelize = require('../db')
 
 const User = sequelize.define('user', {
     id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
     email: { type: DataTypes.STRING, uniqie: true, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false },
-    roles: { type: DataTypes.ARRAY },
-    taskIds: { type: DataTypes.ARRAY}
+    roles: { type: DataTypes.ARRAY(DataTypes.STRING) },
+    taskIds: { type: DataTypes.ARRAY(DataTypes.INTEGER)}
 })
 
 const Task = sequelize.define('task', {
@@ -22,14 +18,10 @@ const Task = sequelize.define('task', {
     date: {type: DataTypes.DATE}
 })
 
-const TasksDay = sequelize.define('taskDate', {
-    Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    date:{type: DataTypes.DATE ,  foreignKey: true },
-    taskIds: { type: DataTypes.ARRAY},
-})
 
-const Note =  sequelize.define('taskDate', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
-    title: { type: DataTypes.STRING, allowNull: false },
-    description: { type: DataTypes.STRING },
-})
+User.hasMany(Task)
+Task.hasOne(User)
+
+module.export = {
+    User, Task
+}
